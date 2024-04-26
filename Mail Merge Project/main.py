cccfb04b5e3a7bda184c7e5ec43a9d10cfc2844f
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-04-26 08:00:37 +0600

    Add files via upload

---
+++ Mail Merge Project/main.py
@@ -0,0 +1,23 @@
+
+
+with open("Containers/Names_container.txt") as Name_File:
+    Name = Name_File.readlines()
+    #for n in Name:
+        #StripedName = n.strip()
+        #print(StripedName)
+
+
+PLACEHOLDER = "[NAME]"
+
+with open("Containers/Letter_container.txt") as Letter_File:
+    Letter = Letter_File.read()
+    #print(Letter)
+
+    for name in Name:
+        stripped_name = name.strip()
+
+        new_letter = Letter.replace(PLACEHOLDER, stripped_name)
+        #print(new_letter)
+
+        with open(f"Output/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
